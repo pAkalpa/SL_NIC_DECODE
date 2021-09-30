d26601f03f8019c74506614409d6ae3e5364a2b3
pasindu <pasinduakalpa1998@gmail.com>
2021-09-30 21:11:19 +0530

    TwelveDigitDecoder Class Added

---
+++ src/me/pasindu/TwelveDigitDecoder.java
@@ -0,0 +1,20 @@
+package me.pasindu;
+
+public class TwelveDigitDecoder {
+    private final int nicLength;
+    private final String nicNumber;
+    private String gender;
+    private String month;
+    private int year;
+    private int day;
+    private int issueData;
+
+    public TwelveDigitDecoder(int nicLength, String nicNumber) {
+        this.nicLength = nicLength;
+        this.nicNumber = nicNumber;
+    }
+
+    public void nicDecode() {
+
+    }
+}
