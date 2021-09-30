11bcf5515b289d071e56ffbeea55f637d26865ac
pasindu <pasinduakalpa1998@gmail.com>
2021-09-30 21:45:06 +0530

    Year Difference Calculation added!

--- src/me/pasindu/TenDigitDecoder.java
+++ src/me/pasindu/TenDigitDecoder.java
@@ -4,6 +4,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class TenDigitDecoder extends TwelveDigitDecoder {
     private final int nicLength;
@@ -23,20 +24,28 @@ public class TenDigitDecoder extends TwelveDigitDecoder {
     @Override
     public void nicDecode() {
         String yearString = nicNumber.substring(0,2);
-        int year = Integer.parseInt(yearString);
+        int year = Integer.parseInt(yearString) + 1900;
 
         String dayOfYearString = nicNumber.substring(2,5);
         int dayOfYear = Integer.parseInt(dayOfYearString);
 
         Calendar calender = Calendar.getInstance();
         calender.set(Calendar.YEAR,year);
-        calender.set(Calendar.DAY_OF_YEAR,dayOfYear);
+        calender.set(Calendar.DAY_OF_YEAR,dayOfYear-1);
+        Calendar calenderNow = Calendar.getInstance();
+
 
         Date date = calender.getTime();
-        DateFormat formatter = new SimpleDateFormat("MMMM dd");
+        Date dateToday = calenderNow.getTime();
+        DateFormat formatter = new SimpleDateFormat("dd MMMM yyyy");
         String dayString = formatter.format(date);
 
-        System.out.println("Year: " + year + "\nDay Of Year: " + dayOfYear + "\n" + dayString);
+        long differenceDate = dateToday.getTime() - date.getTime();
 
+        TimeUnit time = TimeUnit.DAYS;
+        long differenceYears = time.convert(differenceDate, TimeUnit.MILLISECONDS);
+
+//        System.out.println("Year: " + year + "\nDay Of Year: " + dayOfYear + "\n" + dayString);
+        System.out.println(differenceYears/365);
     }
 }
